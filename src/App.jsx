fb4907cf562373a94211bcc3192d073a88458551
Adem-Tozlu <Adem-Tozlu@hotmail.de>
2024-07-15 15:19:45 +0200

    Project mit Tailwind angefangen und alles erstmal vorinstalliert

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-black text-white grid justify-center pb-2 py-1 xl:w-32'>Adem</div>
+
+<div className="grid grid-cols-2 grid-rows-5 gap-4 flex-shrink">
+    <div className="bg-black text-white grid justify-center">1</div>
+    <div >6</div>
+</div>
+    
+    </>
+  )
+}
+
+export default App
